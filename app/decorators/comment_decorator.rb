d4f82007717a6fdfe72ed8cb95185487c11e453b
Nail Alishev <nailalishev@yahoo.com>
2016-04-22 12:07:46 +0300

    comment object decorated

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -1,3 +1,8 @@
 class CommentDecorator < ApplicationDecorator
   delegate_all
+  delegate :full_name, to: :user, prefix: :user
+
+  def published_at
+    "Published at #{object.created_at.to_s(:short)}"
+  end
 end
