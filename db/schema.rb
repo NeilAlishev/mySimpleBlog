6292e710f7925f231265bc6056b6e5752f44052d
Nail Alishev <nailalishev@yahoo.com>
2016-06-14 13:23:45 +0300

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606115814) do
+ActiveRecord::Schema.define(version: 20160608112016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,13 +56,11 @@ ActiveRecord::Schema.define(version: 20160606115814) do
     t.datetime "updated_at",                                      null: false
     t.string   "full_name",              limit: 255
     t.string   "avatar"
-    t.string   "slug"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["full_name"], name: "index_users_on_full_name", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["slug"], name: "index_users_on_slug", using: :btree
 
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
