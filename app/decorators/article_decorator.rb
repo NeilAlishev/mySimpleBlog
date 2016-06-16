1c8f6d8b66c7c5429dfe482fd238942bcba07b91
Nail Alishev <nailalishev@yahoo.com>
2016-06-16 14:50:51 +0300

    add basic styling

--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -1,7 +1,12 @@
 class ArticleDecorator < ApplicationDecorator
-  delegate :id, :title, :content, :user_full_name, :user_avatar
+  delegate :id, :title, :content, :user_full_name, :user_avatar,
+    :comments_count
 
   def published_at
     "Published at #{object.created_at.to_s(:short)}"
+  end
+
+  def comments_count
+    object.comments.count
   end
 end
